7331455f69a3385fb26a2162d34379fa90c77102
Lev <lev@singler.biz>
2019-03-02 15:44:17 +0200

    First version.

---
+++ gmail-tester.js
@@ -0,0 +1,64 @@
+const gmail = require("./gmail");
+const fs = require("fs");
+const path = require("path");
+const { google } = require("googleapis");
+const util = require("util");
+
+async function check_inbox(
+  subject,
+  from,
+  to,
+  wait_time_sec = 30,
+  max_wait_time = 60
+) {
+  try {
+    console.log(
+      `[gmail] Checking for message from '${from}', to: ${to}, contains '${subject}' in subject...`
+    );
+    // Load client secrets from a local file.
+    const content = fs.readFileSync(
+      path.resolve(__dirname, "credentials.json")
+    );
+    const oAuth2Client = await gmail.authorize(JSON.parse(content));
+    const gmail_client = google.gmail({ version: "v1", oAuth2Client });
+    let found_email = null;
+    let done_waiting_time = 0;
+    do {
+      const emails = await gmail.get_recent_email(gmail_client, oAuth2Client);
+      for (let email of emails) {
+        const from_header = email.payload.headers.find(h => h.name === "From");
+        const subject_header = email.payload.headers.find(
+          h => h.name === "Subject"
+        );
+        const delived_to_header = email.payload.headers.find(
+          h => h.name === "Delivered-To"
+        );
+        if (
+          (delived_to_header && delived_to_header.value) === to &&
+          subject_header.value.indexOf(subject) >= 0 &&
+          from_header.value.indexOf(from) >= 0
+        ) {
+          console.log(`[gmail] Found!`);
+          found_email = email;
+          break;
+        }
+      }
+      if (!found_email) {
+        console.log(
+          `[gmail] Message not found. Waiting ${wait_time_sec} seconds...`
+        );
+        done_waiting_time += wait_time_sec;
+        if (done_waiting_time >= max_wait_time) {
+          console.log("[gmail] Maximum waiting time exceeded!");
+          break;
+        }
+        await util.promisify(setTimeout)(wait_time_sec * 1000);
+      }
+    } while (!found_email);
+    return found_email;
+  } catch (err) {
+    return console.log("[gmail] Error:", err);
+  }
+}
+
+module.exports = { check_inbox };
---
+++ gmail.js
@@ -0,0 +1,150 @@
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline");
+const { google } = require("googleapis");
+const util = require("util");
+
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = "token.json";
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ */
+function authorize(credentials) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+  // Check if we have previously stored a token.
+  try {
+    const token = fs.readFileSync(path.resolve(__dirname, TOKEN_PATH));
+    oAuth2Client.setCredentials(JSON.parse(token));
+    return oAuth2Client;
+  } catch (error) {
+    return get_new_token(oAuth2Client);
+  }
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function get_new_token(oAuth2Client) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES
+  });
+  console.log("Authorize this app by visiting this url:", authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question("Enter the code from that page here: ", async code => {
+    rl.close();
+    const getToken = util.promisify(oAuth2Client.getToken);
+    try {
+      const token = getToken(code);
+      oAuth2Client.setCredentials(token);
+      fs.writeFileSync(TOKEN_PATH, JSON.stringify(token));
+      return oAuth2Client;
+    } catch (err) {
+      if (err) return console.error("Error retrieving access token", err);
+    }
+
+    oAuth2Client.getToken(code, (err, token) => {
+      // Store the token to disk for later program executions
+    });
+  });
+}
+
+/**
+ * Lists the labels in the user's account.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+async function list_labels(gmail, oauth2Client) {
+  const list = util.promisify(gmail.users.labels.list);
+  try {
+    const res = await list({
+      userId: "me",
+      auth: oauth2Client
+    });
+    const labels = res.data.labels;
+    return labels;
+  } catch (err) {
+    console.log("The API returned an error: " + err);
+    throw err;
+  }
+}
+
+/**
+ * Retrieve Messages in user's mailbox matching query.
+ *
+ * @param  {String} userId User's email address. The special value 'me'
+ * can be used to indicate the authenticated user.
+ * @param  {String} query String used to filter the Messages listed.
+ */
+async function list_messages(gmail, oauth2Client, query, labelIds) {
+  const list = util.promisify(gmail.users.messages.list);
+  const resp = await list({
+    userId: "me",
+    q: query,
+    auth: oauth2Client,
+    labelIds: labelIds
+  });
+  let result = resp.data.messages;
+  while (resp.nextPageToken) {
+    const resp = await list({
+      userId: "me",
+      q: query,
+      auth: oauth2Client,
+      labelIds: labelIds,
+      pageToken: nextPageToken
+    });
+    result = result.concat(resp.data.messages);
+  }
+  return result;
+}
+
+/**
+ * Get the recent email from your Gmail account
+ *
+ * @param {google.auth.OAuth2} oauth2Client An authorized OAuth2 client.
+ */
+async function get_recent_email(gmail, oauth2Client) {
+  try {
+    const labels = await list_labels(gmail, oauth2Client);
+    const inbox_label_id = [labels.find(l => l.name === "INBOX").id];
+    const messages = await list_messages(
+      gmail,
+      oauth2Client,
+      "",
+      inbox_label_id
+    );
+    let promises = [];
+    const messages_get = util.promisify(gmail.users.messages.get);
+    for (let message of messages) {
+      promises.push(
+        messages_get({ auth: oauth2Client, userId: "me", id: message.id })
+      );
+    }
+    const results = await Promise.all(promises);
+    return results.map(r => r.data);
+  } catch (error) {}
+}
+
+module.exports = {
+  authorize,
+  get_recent_email
+};
