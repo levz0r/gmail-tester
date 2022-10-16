685c4e2ba05e5dd9e9a1869db5bda7593584fdf6
Kyrylo Shmidt <kyrylo@shmidt.od.ua>
2022-10-16 19:02:23 +0300

    Extend Credentials interface

--- gmail-tester.d.ts
+++ gmail-tester.d.ts
@@ -43,6 +43,10 @@ declare module "gmail-tester" {
   export interface Credentials {
     installed : {
       client_id: string,
+      project_id: string,
+      auth_uri: string,
+      token_uri: string,
+      auth_provider_x509_cert_url: string,
       client_secret: string,
       redirect_uris: string[]
     }
